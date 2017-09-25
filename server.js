3506d1ede8281a734ad8004e183a2959efcc1e55
David Hay <david.j.hay@gmail.com>
2017-09-25 12:44:26 +0100

    inserting quotes into db

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var sqlite3 = require('sqlite3');
+var bodyParser = require('body-parser')
+
+//HelloExpress.db has to be in node directory
+var db = new sqlite3.Database('HelloExpress.db');
 var app = express();
 
-//app.use(express.static('public'))
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
 
 //this is better - doesn't care where you run node from :-)
-app.use('/static', express.static(path.join(__dirname, 'public')))
+//app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 app.listen(3000, function(){
      console.log("Server is listening on port 3000");
@@ -19,7 +31,7 @@ app.listen(3000, function(){
  });
  //id required
  app.get("/user/:userid",function(request,response){
- 	 var userid = request.params['userid'];
+ 	var userid = request.params['userid'];
  	response.send("GET USER ["+userid+"]");
  });
  app.delete("/user/:userid",function(request,response){
@@ -30,3 +42,30 @@ app.listen(3000, function(){
  	var userid = request.params['userid'];
  	response.send("UPDATE USER ["+userid+"]");
  });
+
+
+ app.post('/quotes', function(request,response){
+ 	var quote = request.body;
+ 	db.run('INSERT INTO Quotes(quote,author) VALUES (?,?)', quote.quote, quote.author);
+ 	response.send("inserted " + JSON.stringify(quote));
+ });
+
+ app.get('/quotes', function(request,response){
+ 	db.all('SELECT * from Quotes', function(err,rows){
+ 			if (  err ) {
+ 				console.log('ERROR:',err);
+ 			}
+ 			console.log('GET QUOTES: The database currently contains the following: '+rows);
+ 			response.send(rows);
+ 	});
+ });
+ app.get('/quotes/:author', function(request,response){
+ 	db.all('SELECT * from Quotes where Author = ?', [request.params.author], function(err,rows){
+ 			if (  err ) {
+ 				console.log('ERROR:',err);
+ 			}
+ 			console.log('GET QUOTES for author: '+request.params.author);
+ 			response.send(rows);
+ 	});
+ });
+
