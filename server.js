c2001d063ac8095a7cbd0e66e51dbb61ce2249f8
David Hay <david.j.hay@gmail.com>
2017-09-25 11:46:53 +0100

    first commit, basic routes with structure

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'))
+
+app.listen(3000, function(){
+     console.log("Server is listening on port 3000");
+ });
+
+ //no id required
+ app.post("/user",function(request,response){
+ 	response.send("POST USER");
+ });
+ app.get("/user",function(request,response){
+ 	response.send("GET USERS");
+ });
+ //id required
+ app.get("/user/:userid",function(request,response){
+ 	 var userid = request.params['userid'];
+ 	response.send("GET USER ["+userid+"]");
+ });
+ app.delete("/user/:userid",function(request,response){
+ 	var userid = request.params['userid'];
+ 	response.send("DELETE USER ["+userid+"]");
+ });
+ app.put("/user/:userid",function(request,response){
+ 	var userid = request.params['userid'];
+ 	response.send("UPDATE USER ["+userid+"]");
+ });
